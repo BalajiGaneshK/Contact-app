89f8945b0e45365fab6de8f44772d956f27f8741
BalajiGaneshK <kbalajiganeshdav@gmail.com>
2021-06-01 10:31:07 +0530

    Semantic UI overrides app.css

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,12 @@
 import '../App.css';
 import Header from './Header';
-import AddContact from './Add Contact';
-import ContactList from './Contact List';
+/*import AddContact from './Add Contact';
+import ContactList from './Contact List';*/
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <AddContact />
-      <ContactList/>
+      <Header />    
     </div>
   );
 }
